06fd421803509ab7eb2d182917837b83f4312343
Ashutosh Kuinkel <108271619+AshutoshKuinkel@users.noreply.github.com>
2024-12-18 19:00:39 +1100

    Number guessing game

---
+++ Numberguessinggame.py
@@ -0,0 +1,34 @@
+# A simple number guessing game created using Python. Created using the python random module.
+
+import random
+
+top_of_range = input("Type a number: ")
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+if top_of_range <= 0:
+    print("Please type a number larger than 0 nex time.")
+    quit()
+
+random_number = random.randint(0,top_of_range)
+guesses = 0
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number next time.")
+        continue
+
+    if user_guess == random_number:
+        print("You got it! ")
+        break
+    else:
+        if user_guess > random_number:
+            print("You were above the number.")
+        elif user_guess < random_number:
+            print("You were below the number.")
+        print("You got it wrong.")
+
+print("You got it in",guesses,"guesses! ")
